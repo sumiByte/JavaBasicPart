b1a98d3a5c547ccbb0c29c64f133e2e76cdc6a22
sumiByte <sumishanghai2023@proton.me>
2023-06-15 19:42:20 +0800

    Here we've looked at the concept of polymorphism and its usage, and the point of attention, which has something to do with inheritance

--- src/TextObjectoriented.java
+++ src/TextObjectoriented.java
@@ -3,21 +3,22 @@ public class TextObjectoriented {
     public static void main(String[] args) {
 
 
-        //在这里我一般会创建相对应的类的对象
+        //多态：可以将父类的变量指向子类的对象(子类的对象可以赋给父类的引用)
+        /*在之前的创建对象中我们将父类的对象赋给父类引用，把子类的对象赋给子类引用
         Pople pople = new Pople();
-            /*在这里我们将Pople这个对象给了pople这个引用，也就是父类的引用
-            我们可以访问父类中的所有属性和方法
-             */
-        pople.popleName = "张三";
-        pople.popleFun();
-
-        /*当然我们创建的子类Student继承了父类，它也可以访问到父类Pople中的属性和方法
-        比如以下这个例子
-         */
-
         Student student = new Student();
 
-        student.popleName = "李四";
-        student.popleFun();
+        但是现在，我们把子类的对象赋给父类的引用
+         */
+
+        Pople pople = new Student();
+
+        pople.popleName = "王五";
+        pople.popleFun();
+
+        /*在这里我们发现虽然我们把子类的对象赋给了父类的引用，但是我们还是无法方法问到
+        子类中的studentFun();方法，这是因为在父类中并没有这个方法，父类只能方法问到子类继承
+        父类的方法和属性，而无法访问子类自己的属性和方法
+         */
     }
 }
